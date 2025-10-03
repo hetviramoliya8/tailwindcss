ec27df5341f47977e5c5fa8bbf791cfb7a7aaaf8
hetviramoliya8 <ramoliyahetvi8@gmail.com>
2025-10-03 10:30:52 +0530

    practies

---
+++ Sugar/script.js
@@ -0,0 +1,56 @@
+function getData(){
+    const Products = [
+  {
+    id: "P001",
+    name: "Matte Attack Transferproof Lipstick",
+    price: 699,
+    discountPrice: 499,
+    category: "Lips",
+    rating: 4.5,
+    image: "https://cdn.sugarcosmetics.com/products/1234.jpg",
+    inStock: true,
+    shades: ["01 Bold", "02 Red Rush", "03 Mauve"],
+    tags: ["bestseller", "transferproof", "long-lasting"]
+  },
+  {
+    id: "P002",
+    name: "Ace Of Face Foundation Stick",
+    price: 999,
+    discountPrice: 799,
+    category: "Face",
+    rating: 4.3,
+    image: "https://cdn.sugarcosmetics.com/products/2234.jpg",
+    inStock: true,
+    shades: ["10 Latte", "15 Cappuccino", "20 Mocha"],
+    tags: ["stick", "foundation", "SPF"]
+  },
+  {
+    id: "P003",
+    name: "Contour De Force Face Palette",
+    price: 1099,
+    discountPrice: 899,
+    category: "Face Palette",
+    rating: 4.7,
+    image: "https://cdn.sugarcosmetics.com/products/3344.jpg",
+    inStock: false,
+    shades: ["Subtle Summit", "Vivid Victory"],
+    tags: ["contour", "blush", "highlight"]
+  },
+  {
+    id: "P004",
+    name: "Uptown Curl Mascara - Mini",
+    price: 499,
+    discountPrice: 399,
+    category: "Eyes",
+    rating: 4.2,
+    image: "https://cdn.sugarcosmetics.com/products/4455.jpg",
+    inStock: true,
+    shades: [],
+    tags: ["mascara", "curl", "mini"]
+  }
+];
+
+}
+
+
+ 
